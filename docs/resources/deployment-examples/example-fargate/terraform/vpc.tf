7dc4584d774cefebf1d606d960f753de379de4be
Jose Luis Aguilar <joseluis.aguilar@idt.net>
2022-07-26 12:58:00 -0600

    VPC changes

--- docs/resources/deployment-examples/example-fargate/terraform/vpc.tf
+++ docs/resources/deployment-examples/example-fargate/terraform/vpc.tf
@@ -13,10 +13,10 @@ module "vpc" {
   version = "3.2.0"
 
   name                 = "${var.environment}-anchorplatform-fargatedemo-vpc"
-  cidr                 = "10.0.0.0/16"
+  cidr                 = "10.100.0.0/16"
   azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  private_subnets      = ["10.100.10.0/26", "10.100.10.64/26", "10.100.10.128/26"]
+  public_subnets       = ["10.100.12.0/26", "10.100.12.64/26", "10.100.12.128/26"]
   enable_nat_gateway   = true
   single_nat_gateway   = false
   reuse_nat_ips        = true
